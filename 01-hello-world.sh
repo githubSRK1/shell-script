3f94707465712f3c710665af8ebe5a434a931ee3
SRK <RanjithSrikonda.Devops@example.com>
2023-08-16 22:09:57 +0530

    hello world script added

--- 01-hello-world.sh
+++ 01-hello-world.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
 echo "Hello world, I am learning Devops with AWS"
+
+echo "Ramesh : Hi Suresh, Good Morning"
+echo "Suresh : Good Morning Ramesh"
+echo "Ramesh : How are you Suresh"
+echo "Suresh : I am good, Hpw are you Ramesh"
