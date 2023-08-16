0d1fc8201542a9615eb9bfb75a035d4a947c61a7
SRK <RanjithSrikonda.Devops@example.com>
2023-08-16 22:13:32 +0530

    hello world script added

--- 01-hello-world.sh
+++ 01-hello-world.sh
@@ -2,7 +2,9 @@
 
 echo "Hello world, I am learning Devops with AWS"
 
-echo "Ramesh : Hi Suresh, Good Morning"
-echo "Suresh : Good Morning Ramesh"
-echo "Ramesh : How are you Suresh"
-echo "Suresh : I am good, Hpw are you Ramesh"
+PERSON1=$1
+PERSON2=$2
+echo "$PERSON1 : Hi $PERSON2, Good Morning"
+echo "$PERSON2 : Good Morning $PERSON1"
+echo "$PERSON1 : How are you $PERSON2"
+echo "$PERSON2 : I am good, Hpw are you $PERSON1"
