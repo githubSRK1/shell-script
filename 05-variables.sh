87b557038ed0013004a53a308aada1eede51e4e5
SRK <RanjithSrikonda.Devops@example.com>
2023-08-16 22:34:01 +0530

    hello world script added

--- 05-variables.sh
+++ 05-variables.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "please enter your username"
-read USERNAME
+read -s USERNAME
 echo "username entered is : $USERNAME"
 echo "please enter your password"
-read PASSWORD
+read -s PASSWORD
 echo "username entered is : $PASSWORD"
