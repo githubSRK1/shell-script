690ed7204556f6c82441081351a901f3cdab286a
SRK <RanjithSrikonda.Devops@example.com>
2023-08-17 18:28:29 +0530

    added condition

---
+++ 08-install-git.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# our program is to install mysql
+
+USERID=$(id-u)
+
+if [ $USERID -ne 0]
+then 
+    echo "ERROR:: Please run this script with root access"
+# else
+#     echo "INFO :: You are root user"
+fi
+
+yum install git -y
+
